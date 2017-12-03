87f90721b2e6be148010e041b8347432a7e0756b
MaraAnn <mara@i2pmail.org>
2017-12-03 00:38:02 -0600

    test

--- base64encode.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import requests,os,sys
-if sys.argv[1] != None:
- try:
-  print requests.post("https://ghostbin.com/paste/new",data={"text": open(str(sys.argv[1]), 'rb').read().encode('base64'),"expire":"-1","lang":"text"},verify=False).url
- except:
-  print "Usage:\r\nbase64Encode.py {filename}\r\n-----------------------\r\nThis is available as both full location and location compared to your current location"
--- masslookups.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from netaddr import IPNetwork
-import socket, os
-
-list=map(str, str(raw_input("Add comma seperated ip ranges: ")).split(","))
-for i in list:
-  for ip in IPNetwork(i):
-   open('iplist-%s.log' % i.replace('/',''), 'a').write(str(ip)+","+str(socket.gethostbyaddr(str(ip))[0])+"\n")
-
